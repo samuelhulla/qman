5514dd062e2b559e60464d44507f4cb5667c2e2b
samuelhulla <oficialny.hulla@gmail.com>
2023-08-17 11:37:53 +0200

    📚 Docs: Schema

--- core/src/schema.ts
+++ core/src/schema.ts
@@ -9,6 +9,21 @@ type Schema<Key extends string, T extends QueryMap<T>> = {
 	[K in keyof T as `${Key}/${K extends string ? K : never}`]: T[K]
 }
 
+/**
+ * Creates a new schema object with the specified key and query map.
+ * @param key - The key for the schema.
+ * @param queryMap - An object that maps query keys to query functions.
+ * @returns A new schema object that maps flattened query keys to query functions.
+ * @template K - The type of the schema key.
+ * @template T - The type of the query map.
+ * @example
+ * ```ts
+ * const user = schema('user', {
+ *   get: query((id: string) => ({ id, name, pic })),
+ *   profilePic: query((id: string) => ({ id, name, pic }), ({ pic }) => pic),
+ * })
+ * ```
+ */
 export function schema<K extends string, T extends QueryMap<T>>(key: K, queryMap: T): Schema<K, T> {
 	return entries(queryMap).reduce(
 		(acc, [queryKey, queryFn]) => ({
