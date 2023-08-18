49f721a5c232da6452e30c8af245e0e02c04086d
samuelhulla <oficialny.hulla@gmail.com>
2023-08-18 17:40:10 +0200

    🐞 Fix: TS Correct extraction of generic arguments in Unsubscribe

--- swr/src/subscribe.ts
+++ swr/src/subscribe.ts
@@ -1,14 +1,17 @@
-import type { SWRSubscription, SWRSubscriptionOptions } from "swr/subscription"
+import type { SWRSubscription, SWRSubscriptionOptions, SWRSubscriptionResponse } from "swr/subscription"
 import useSWRSub from "swr/subscription"
 
+type Next = <Error, Data>(error: Error, data?: Data) => void
+type SWRNext<T, E> = SWRSubscriptionOptions<T, E>
 type Key = Parameters<typeof useSWRSub>[0]
-type SWRError<E = any> = E | null | undefined
 
-type Updater<T, R = unknown, E = SWRError> = (value: T, next: SWRSubscriptionOptions<T, E>["next"]) => R
+type Updater<T> = <N extends Next>(value: T, next: N) => any
+type Fn = (...args: any[]) => any
+type UpdaterResult<U> = U extends (...args: any[]) => infer R ? (R extends Fn ? ReturnType<R> : R) : U
 
-export function subscribe<T, K extends Key, E = SWRError, U extends Updater<T> = Updater<T, E>>(col: T, key: K, updater: U) {
-	const fn = (_: K, { next }: SWRSubscriptionOptions<T, E>) => {
-		updater(col, next)
-	}
-	return useSWRSub(key, fn as SWRSubscription<K, ReturnType<typeof updater>, E>)
+export function subscribe<T, K extends Key, U extends Updater<T>, E = Error>(value: T, queryKey: K, updater: U) {
+	const observer = (_key: K, { next }: SWRNext<T, E>) => updater(value, next as Next)
+	const res = useSWRSub(queryKey, observer as SWRSubscription<K, T, E>)
+	return res as SWRSubscriptionResponse<UpdaterResult<U>, E>
 }
+
