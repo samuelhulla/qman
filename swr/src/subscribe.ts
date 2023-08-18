d614b0bc63ec31771a253ba855f601ea33233b0f
samuelhulla <oficialny.hulla@gmail.com>
2023-08-18 10:53:32 +0200

    🐞 Fix: TS Updater Return being void (override) + SWRError not being enough to cover default Error type

--- swr/src/subscribe.ts
+++ swr/src/subscribe.ts
@@ -2,13 +2,14 @@ import type { SWRSubscription, SWRSubscriptionOptions } from "swr/subscription"
 import useSWRSub from "swr/subscription"
 
 type Key = Parameters<typeof useSWRSub>[0]
-type SWRError = Error | null | undefined
+type SWRError = Error | null
 
-type Updater<R, T, E = SWRError> = (value: T, next: SWRSubscriptionOptions<T, E>["next"]) => R
+type Updater<T, E = SWRError> = (value: T, next: SWRSubscriptionOptions<T, E>["next"]) => any
+type UpdaterReturn<U> = U extends (...args: any[]) => infer R ? R : never
 
-export function subscribe<R, T, K extends Key, E = SWRError, U extends Updater<R, T, E> = Updater<R, T, E>>(col: T, key: K, updater: U) {
+export function subscribe<T, K extends Key, E = SWRError, U extends Updater<T, E> = Updater<T, E>>(col: T, key: K, updater: U) {
 	const fn = (_: K, { next }: SWRSubscriptionOptions<T, E>) => {
 		updater(col, next)
 	}
-	return useSWRSub(key, fn as SWRSubscription<K, ReturnType<U>, E>)
+	return useSWRSub(key, fn as SWRSubscription<K, UpdaterReturn<U>, E>)
 }
