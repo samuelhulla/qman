8acafb9cbb1656b8b508be4877160817cedf769f
samuelhulla <oficialny.hulla@gmail.com>
2023-08-21 10:53:38 +0200

    ✳️ Update: Schema now accepts new query style, gets rid of unnecessary redundant [key]: query(key, ...) declaration

--- core/src/schema.ts
+++ core/src/schema.ts
@@ -1,29 +1,23 @@
-import { entries } from "./util"
-import type { Fn } from "./query"
+import type { Query, Fn } from "./query"
 
-type QueryMap<T> = {
-	[K in keyof T]: T[K] extends Fn ? T[K] : never
+type KeyFnPair = [string, Fn]
+
+type SchemaMap<Q> = Q extends KeyFnPair[] ? { [T in Q[number] as T[0]]: T[1] } : never
+
+type QueryArgs<Q, K> = Q extends KeyFnPair[] ? (K extends keyof SchemaMap<Q> ? Parameters<SchemaMap<Q>[K]> : never) : never
+
+type QueryResult<Q, K> = Q extends KeyFnPair[]
+  ? K extends keyof SchemaMap<Q>
+    ? SchemaMap<Q>[K] extends Query<Fn, infer R>
+      ? R
+      : never
+    : never
+  : never
+
+type Schema<S, G> = {
+  schema: S
+  get: G
 }
-
-type SchemaHash<Key extends string, T extends QueryMap<T>> = {
-	[K in keyof T as `${Key}/${K extends string ? K : never}`]: T[K]
-}
-
-type QueryArgs<T extends QueryMap<T>, Key extends keyof T> = {
-	[K in keyof T]: T[K] extends Fn ? Parameters<ReturnType<T[K]>> : never
-}[Key]
-
-type QueryResult<T extends QueryMap<T>, Key extends keyof T> = {
-	[K in keyof T]: T[K] extends Fn ? (ReturnType<T[K]> extends Fn ? ReturnType<ReturnType<T[K]>> : never) : never
-}[Key]
-
-type Schema<S, G, P> = {
-	schema: S
-	get: G
-	prepare: P
-}
-
-type HashedKey<K extends string, Q, Args extends readonly unknown[]> = [`${K}/${Q extends string ? Q : never}`, ...Args]
 
 /**
  * Creates a new schema object with the specified key and query map.
@@ -34,35 +28,32 @@ type HashedKey<K extends string, Q, Args extends readonly unknown[]> = [`${K}/${
  * @template T - The type of the query map.
  * @example
  * ```ts
- * const user = schema('user', {
- *   get: query((id: string) => ({ id, name, pic })),
- *   profilePic: query((id: string) => ({ id, name, pic }), ({ pic }) => pic),
- * })
+ * const user = schema('user',
+ *   query('get', (id: string) => ({ id, name, picture })),
+ *   query('picture', (id: string) => ({ id, name, picture }), ({ picture }) => picture),
+ * )
  * ```
  */
-export function schema<K extends string, T extends QueryMap<T>>(key: K, queryMap: T) {
-	const schemaHash = entries(queryMap).reduce(
-		(acc, [queryKey, queryFn]) => ({
-			...acc,
-			[`${key}/${queryKey.toString()}`]: queryFn,
-		}),
-		{} as SchemaHash<K, T>,
-	)
-	/**
-	 * Retrieves a query function from the specified schema.
-	 * @param key - The key of the query function to retrieve.
-	 * @param queryArgs - The arguments to pass to the query function.
-	 * @returns An object containing the query function, query key, and schema key.
-	 * @template K - The type of the query key.
-	 */
-	const get = <Key extends keyof T>(queryKey: Key, args: QueryArgs<T, Key>) => queryMap[queryKey](key, ...args) as QueryResult<T, Key>
-	const prepare = <Key extends keyof T>(queryKey: Key, args: QueryArgs<T, Key>) => ({
-		queryFn: () => queryMap[queryKey](key, ...args) as (key: K, ...args: QueryArgs<T, Key>) => QueryResult<T, Key>,
-		queryKey: [`${key as K}/${queryKey.toString()}`, ...args] as HashedKey<K, Key, typeof args>,
-	})
-	return {
-		get,
-		prepare,
-		schema: schemaHash,
-	} as Schema<typeof schemaHash, typeof get, typeof prepare>
+export function schema<SK extends string, Q extends KeyFnPair[]>(schemaKey: SK, ...queries: Q) {
+  const schemaMap = queries.reduce(
+    (acc, [queryKey, queryFn]) => ({
+      ...acc,
+      [queryKey]: queryFn,
+    }),
+    {} as SchemaMap<Q>,
+  )
+  /**
+   * Retrieves a query function from the specified schema.
+   * @param key - The key of the query function to retrieve.
+   * @param queryArgs - The arguments to pass to the query function.
+   * @returns An object containing the query function, query key, and schema key.
+   * @template K - The type of the query key.
+   */
+  const get = <Key extends keyof SchemaMap<Q>>(queryKey: Key, args: QueryArgs<Q, Key>) =>
+    schemaMap[queryKey]?.(schemaKey, ...args) as QueryResult<Q, Key>
+  return {
+    get,
+    schema: schemaMap,
+    schemaKey,
+  } as Schema<typeof schemaMap, typeof get>
 }
