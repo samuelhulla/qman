a157f989a0e66dccee8d1288d7f670d5dd522dfd
samuelhulla <oficialny.hulla@gmail.com>
2023-08-21 12:02:41 +0200

    🎨 Format: Use spaces over tabs for better git file preview

--- swr/src/subscribe.ts
+++ swr/src/subscribe.ts
@@ -10,7 +10,7 @@ type Fn = (...args: any[]) => any
 type UpdaterResult<U> = U extends (...args: any[]) => infer R ? (R extends Fn ? ReturnType<R> : R) : U
 
 export function subscribe<T, K extends Key, U extends Updater<T>, E = Error>(value: T, queryKey: K, updater: U) {
-	const observer = (_key: K, { next }: SWRNext<T, E>) => updater(value, next as Next)
-	const res = useSWRSub(queryKey, observer as SWRSubscription<K, T, E>)
-	return res as SWRSubscriptionResponse<UpdaterResult<U>, E>
+  const observer = (_key: K, { next }: SWRNext<T, E>) => updater(value, next as Next)
+  const res = useSWRSub(queryKey, observer as SWRSubscription<K, T, E>)
+  return res as SWRSubscriptionResponse<UpdaterResult<U>, E>
 }
