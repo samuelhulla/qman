89f21822992748cfdfcd3429fbf4a0544160c6f8
samuelhulla <oficialny.hulla@gmail.com>
2023-08-24 15:04:10 +0200

    ✳️ Update: Allow pagination data

--- swr/src/data.ts
+++ swr/src/data.ts
@@ -1,7 +1,14 @@
 import type { SWRSubscriptionResponse } from "swr/subscription"
 import type { SWRResponse } from "swr"
+import type { SWRInfiniteResponse } from "swr/infinite"
 
-type DataFromSWR<R> = R extends SWRSubscriptionResponse<infer D, any> ? D : R extends SWRResponse<infer D, any> ? D : R
+type DataFromSWR<R> = R extends SWRSubscriptionResponse<infer D, any>
+  ? D
+  : R extends SWRResponse<infer D, any>
+  ? D
+  : R extends SWRInfiniteResponse<infer D, any>
+  ? D
+  : R
 
 export function data<R extends SWRSubscriptionResponse | SWRResponse>(response: R): DataFromSWR<R> {
   return response.data
