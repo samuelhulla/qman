b7db238a0b6a24eafcfd275b68aedfd7a16953dc
samuelhulla <oficialny.hulla@gmail.com>
2024-01-02 13:22:26 +0100

    🔧 Update: use types

--- swr/src/use.ts
+++ swr/src/use.ts
@@ -1,7 +1,9 @@
 import useSWR from "swr"
+import type { PublicConfiguration, BareFetcher } from "swr/_internal"
 
 type Key = Parameters<typeof useSWR>[0]
+type Options<T> = Partial<PublicConfiguration<T, any, BareFetcher<T>>>
 
-export function use<T, K extends Key>(value: Promise<T>, queryKey: K) {
-  return useSWR(queryKey, () => value)
+export function use<T, K extends Key>(value: T, queryKey: K, queryOptions?: Options<T>) {
+  return useSWR(queryKey, () => value, queryOptions)
 }
