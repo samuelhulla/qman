a2f6699e7c1f8bd4c729575d11ef6c1f70376757
samuelhulla <oficialny.hulla@gmail.com>
2024-01-19 18:03:37 +0100

    🔧 Update: type exports

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,4 +1,4 @@
-export { call, type Call } from "./call"
-export { query, type Query } from "./query"
+export { call, type Call, type Args, type Getter, type Invocation, type Methods } from "./call"
+export { query, type Query, type Modifier } from "./query"
 export { schema, type Schema, type QueryArgs, type QueryReturn } from "./schema"
 export { infiniteQuery, type InfiniteQuery } from "./infiniteQuery"
