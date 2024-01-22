1c79e32f88da5cd47e46daf2aa4c66b452473799
samuelhulla <oficialny.hulla@gmail.com>
2024-01-22 18:31:44 +0100

    ✅ Feat: axios request

--- axios/src/request.ts
+++ axios/src/request.ts
@@ -1,15 +1,22 @@
 import { type AxiosRequestConfig } from "axios"
 import { type Args, type Getter, type Methods } from "qman"
 
-export type ConfigWithMethod<M extends Methods> = AxiosRequestConfig & { method: M }
-export type ConfigOrConfigFunction<A extends Args, M extends Methods> = ConfigWithMethod<M> | ((...args: A) => ConfigWithMethod<M>)
-export type Resolver<M extends Methods, R> = (config: ConfigWithMethod<M>) => R
+type Config<C extends AxiosRequestConfig> = {
+  [K in keyof C]: C[K] extends "method" ? (C[K] extends Methods ? C[K] : "GET") : C[K]
+}
 
-export function request<M extends Methods, R, A extends Args = Args>(
-  config: ConfigOrConfigFunction<A, M>,
-  resolver: Resolver<M, R>,
-): Getter<A, R, M> {
-  const configFn = typeof config === "function" ? config : () => config
-  const getter = <SK extends string>(_schemaKey: SK, ...args: A) => resolver(configFn(...args))
-  return getter as unknown as Getter<A, R, M>
+type Method<C extends AxiosRequestConfig> = C extends { method: infer M } ? (M extends Methods ? M : "GET") : "GET"
+
+/**
+ * Special function that creates a special getter with help of axios
+ * @param config Function that returns axios config (use params to create dynamic config)
+ * @param instance The direct axios instance function, that takes config as first argument
+ * @returns Standard query `Getter` that returns axios data fetch with proper method specified
+ */
+export function request<A extends Args, R, C extends AxiosRequestConfig>(
+  config: (...args: A) => Config<C>,
+  instance: (config: Config<C>) => R,
+): Getter<A, R, Method<C>> {
+  const getter = <SK extends string>(_schemaKey: SK, ...args: A) => instance(config(...args))
+  return getter as unknown as Getter<A, R, Method<C>>
 }
