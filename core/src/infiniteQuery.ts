be76e7cfcfd42faa22ce0e20324ad6cbfa30d619
samuelhulla <oficialny.hulla@gmail.com>
2024-01-22 18:32:40 +0100

    📚 Docs: infiniteQuery

--- core/src/infiniteQuery.ts
+++ core/src/infiniteQuery.ts
@@ -1,11 +1,18 @@
-import type { Getter, Args, Invocation, Methods } from "./call"
+import type { Getter, Args, Query, Methods } from "./query"
 import type { HashedQueryKey } from "./query"
 
 type PageParams<A extends Args> = Parameters<(page: number, pageSize: number, ...args: A) => any>
 type Paginator<QK extends string, A extends Args, R1, R> = (arg: R1, hashKey: HashedQueryKey<string, QK, A>) => R
 
-export type InfiniteQuery<K extends string, A extends Args, R, M extends Methods> = Invocation<K, PageParams<A>, R, "infiniteQuery", M>
+export type InfiniteQuery<K extends string, A extends Args, R, M extends Methods> = Query<K, PageParams<A>, R, "infiniteQuery", M>
 
+/**
+ * A query modifier that requires two special pagination params
+ * @param queryKey The standard query key
+ * @param fn The getter function (promise/axios/fetch)
+ * @param paginator Special paginator that takes state into consideration
+ * @returns An infinite query
+ */
 export function infiniteQuery<QK extends string, A extends Args, R1, R, M extends Methods>(
   queryKey: QK,
   fn: Getter<A, R1, M>,
@@ -13,5 +20,5 @@ export function infiniteQuery<QK extends string, A extends Args, R1, R, M extend
 ): InfiniteQuery<QK, A, R, M> {
   const getter = <SK extends string>(schemaKey: SK, ...args: A) =>
     paginator(fn(...(args.slice(2) as unknown as A)), [`${schemaKey}/${queryKey}`, ...args])
-  return { key: queryKey, getter: getter as unknown as Getter<PageParams<A>, R>, type: "infiniteQuery" }
+  return { key: queryKey, getter: getter as unknown as Getter<PageParams<A>, R, M>, type: "infiniteQuery" }
 }
