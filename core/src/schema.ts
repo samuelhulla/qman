295c605fc20689edbedcd60bf86d04647472462d
samuelhulla <oficialny.hulla@gmail.com>
2024-01-22 18:33:20 +0100

    🔧 Update: Warn on `.call` querykey overlap

--- core/src/schema.ts
+++ core/src/schema.ts
@@ -1,14 +1,14 @@
-import type { Invocation, Methods } from "./call"
+import { type Query, type Methods } from "./query"
 
 export type Key<Q> = Queries<Q>[number]["key"]
 export type MethodKeys<Q, M extends Methods> = QueriesWithMethod<Q, M>[number]["key"]
 
-export type Queries<Q> = Q extends [Invocation<infer K, infer A, infer R, infer T, infer M>, ...infer Qs]
-  ? [Invocation<K, A, R, T, M>, ...Queries<Qs>]
+export type Queries<Q> = Q extends [Query<infer K, infer A, infer R, infer T, infer M>, ...infer Qs]
+  ? [Query<K, A, R, T, M>, ...Queries<Qs>]
   : []
-export type QueriesWithMethod<Q, M extends Methods> = Q extends [Invocation<infer K, infer A, infer R, infer T, infer Method>, ...infer Qs]
+export type QueriesWithMethod<Q, M extends Methods> = Q extends [Query<infer K, infer A, infer R, infer T, infer Method>, ...infer Qs]
   ? Method extends M
-    ? [Invocation<K, A, R, T, M>, ...QueriesWithMethod<Qs, M>]
+    ? [Query<K, A, R, T, M>, ...QueriesWithMethod<Qs, M>]
     : QueriesWithMethod<Qs, M>
   : []
 
@@ -21,14 +21,14 @@ export type QueryMapWithMethod<Q, M extends Methods> = {
 
 export type QueryArgs<Q, K extends Key<Q>> = Q extends Queries<Q> ? (K extends Key<Q> ? QueryMap<Q>[K] : never) : never
 export type QueryArgsWithMethod<Q, M extends Methods, K extends Key<Q>> = {
-  [QK in QueriesWithMethod<Q, M>[number] as QK["key"]]: QK extends Invocation<QK["key"], infer A, any, any, M> ? A : never
+  [QK in QueriesWithMethod<Q, M>[number] as QK["key"]]: QK extends Query<QK["key"], infer A, any, any, M> ? A : never
 }[K]
 
 export type QueryReturn<Q, QK extends Key<Q>> = {
-  [K in Queries<Q>[number] as K["key"]]: K extends Invocation<K["key"], any, infer R, any, any> ? Awaited<R> : never
+  [K in Queries<Q>[number] as K["key"]]: K extends Query<K["key"], any, infer R, any, any> ? Awaited<R> : never
 }[QK]
 export type QueryReturnWithMethod<Q, QK extends Key<Q>, M extends Methods> = {
-  [K in QueriesWithMethod<Q, M>[number] as K["key"]]: K extends Invocation<K["key"], any, infer R, any, M> ? Awaited<R> : never
+  [K in QueriesWithMethod<Q, M>[number] as K["key"]]: K extends Query<K["key"], any, infer R, any, M> ? Awaited<R> : never
 }[QK]
 
 type SchemaCall<Q, M extends Methods> = <K extends MethodKeys<Q, M>>(
@@ -59,21 +59,23 @@ type UnusedMethods<Q> = {
 }[keyof SchemaDefinition<Q>]
 export type Schema<Q> = CommonMethods<Q> & Omit<SchemaDefinition<Q>, UnusedMethods<Q>>
 
-export function schema<const K, Q extends Invocation<any, any, any, any, any>[]>(schemaKey: K, ...queries: Q): Schema<Q> {
+export function schema<const K, Q extends Query<any, any, any, any, any>[]>(schemaKey: K, ...queries: Q): Schema<Q> {
   const key = (queryKey: Key<Q>) => `${schemaKey}/${queryKey}`
   const call = <QK extends Key<Q>, A extends QueryArgs<Q, QK>>(queryKey: QK, args: A) => {
-    const match = queries.find(q => q.key === queryKey)
-    return match?.type === "query"
-      ? (match?.getter(`${schemaKey}/${queryKey}`, ...args) as QueryReturn<Q, QK>)
-      : (match?.getter(...args) as QueryReturn<Q, QK>)
+    const matches = queries.filter(q => q.key === queryKey)
+    if (matches.length > 1) {
+      console.warn(
+        `[qman]: Multiple queries with key ${queryKey} found in schema ${schemaKey}. Using the first one. If you have endpoints with same key but different methods specified use the schema.method syntax`,
+      )
+    }
+    const match = matches[0]
+    return match?.getter(`${schemaKey}/${queryKey}`, ...args) as QueryReturn<Q, QK>
   }
   const methodCall =
     <M extends Methods>(_method: M): SchemaCall<Q, M> =>
     <QK extends MethodKeys<Q, M>, A extends QueryArgsWithMethod<Q, M, QK>>(queryKey: QK, args: A) => {
       const match = queries.find(q => q.key === queryKey)
-      return match?.type === "query"
-        ? (match?.getter(`${schemaKey}/${queryKey}`, ...args) as QueryReturnWithMethod<Q, QK, M>)
-        : (match?.getter(...args) as QueryReturnWithMethod<Q, QK, M>)
+      return match?.getter(`${schemaKey}/${queryKey}`, ...args) as QueryReturnWithMethod<Q, QK, M>
     }
   return {
     key,
