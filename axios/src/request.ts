086bf2d87801011a0cc2bb49f5540879265ed915
samuelhulla <oficialny.hulla@gmail.com>
2024-01-25 09:36:07 +0100

    🔧 Update request query equivalent

--- axios/src/request.ts
+++ axios/src/request.ts
@@ -1,11 +1,8 @@
 import { type AxiosRequestConfig } from "axios"
-import { type Args, type Getter, type Methods } from "qman"
+import type { Query } from "qman"
+import { type Args, type Getter, type Methods, type Modifier } from "qman"
 
-type Config<C extends AxiosRequestConfig> = {
-  [K in keyof C]: C[K] extends "method" ? (C[K] extends Methods ? C[K] : "GET") : C[K]
-}
-
-type Method<C extends AxiosRequestConfig> = C extends { method: infer M } ? (M extends Methods ? M : "GET") : "GET"
+type Config<M extends Methods> = AxiosRequestConfig & { method: M }
 
 /**
  * Special function that creates a special getter with help of axios
@@ -13,10 +10,25 @@ type Method<C extends AxiosRequestConfig> = C extends { method: infer M } ? (M e
  * @param instance The direct axios instance function, that takes config as first argument
  * @returns Standard query `Getter` that returns axios data fetch with proper method specified
  */
-export function request<A extends Args, R, C extends AxiosRequestConfig>(
-  config: (...args: A) => Config<C>,
-  instance: (config: Config<C>) => R,
-): Getter<A, R, Method<C>> {
-  const getter = <SK extends string>(_schemaKey: SK, ...args: A) => instance(config(...args))
-  return getter as unknown as Getter<A, R, Method<C>>
+export function request<QK extends string, A extends Args, R1, const M extends Methods, R>(
+  queryKey: QK,
+  configFn: (...args: A) => Config<M>,
+  instance: (config: Config<M>) => R1,
+  modifier?: Modifier<QK, A, R1, R>,
+  // @ts-expect-error
+): Query<QK, A, R, typeof modifier extends undefined ? "query" : "modifiedQuery", M> {
+  let method: M = "GET" as M
+  const getter = <SK extends string>(schemaKey: SK, ...args: A) => {
+    const config = configFn(...args)
+    method = config.method ?? method
+    return modifier ? modifier(instance(config), [`${schemaKey}/${queryKey}`, ...args]) : instance(config)
+  }
+
+  return {
+    key: queryKey,
+    getter: getter as unknown as Getter<A, typeof modifier extends undefined ? R1 : R, M>,
+    // @ts-expect-error
+    type: modifier ? "modifiedQuery" : "query",
+    method,
+  }
 }
